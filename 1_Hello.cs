27f870a1ba23a24385ae32c3d0962fa54679a85c
YasirHaris <yasir.haris@gmail.com>
2019-05-21 11:21:35 +0700

    Create 1_Hello.cs
    Hello world

---
+++ 1_Hello.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace code2heaven
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
